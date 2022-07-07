3ca5d47efac60811092a8fa8cd2ab01ffcafbe81
Fabibourdokan <fabianabourdokanvaliente@gmail.com>
2022-07-07 20:01:26 -0300

    desafio entregue

---
+++ script.js
@@ -0,0 +1,17 @@
+let dados = ['Daniel','Clarinha','Rafa','Andressa','Lucas'];
+console.log(dados);
+
+dados.unshift('Play');
+console.log(dados);
+
+dados.pop();
+console.log(dados);
+
+dados.push('Luiza','Mel');
+console.log(dados);
+
+dados.shift();
+console.log(dados);
+
+let number = [7,5,6,3,8,9,2,1,4]
+console.log(number.sort());
